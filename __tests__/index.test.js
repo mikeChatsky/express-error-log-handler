6f8725be7ebc260999830b06254388fb33f271a6
Mike Chatsky <mike20016@gmail.com>
2019-04-25 23:51:12 +0300

    test(status): add status code tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,10 @@
 import { mockRequest, mockResponse } from 'mock-req-res';
 import errorHandler from '../src';
 
+const INTERNAL_SERVER_ERROR = 500;
+
+const noop = () => {};
+
 const next = jest.fn();
 
 const logger = jest.fn(err => err);
@@ -14,5 +18,19 @@ describe('Middleware structure', () => {
     const returned = errorHandler(logger);
 
     expect(returned instanceof Function).toBeTruthy();
+  });
+});
+
+describe('Status codes', () => {
+  const handler = errorHandler(logger);
+
+  test('default', () => {
+    const res = mockResponse();
+
+    handler(new Error('Some error'), {}, res, noop);
+
+    expect(
+      res.json.calledWithMatch({ statusCode: INTERNAL_SERVER_ERROR })
+    ).toBeTruthy();
   });
 });
