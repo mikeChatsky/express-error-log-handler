9e4c4872c0dda590ba76adb60b5ae484e36def88
Mike Chatsky <mike20016@gmail.com>
2019-04-27 02:19:18 +0300

    fix(test): re-organized tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,9 @@
-import { mockRequest, mockResponse } from 'mock-req-res';
+import { mockResponse } from 'mock-req-res';
 import errorHandler from '../src';
 
 const INTERNAL_SERVER_ERROR = 500;
 
 const noop = () => {};
-
-const next = jest.fn();
 
 const logger = jest.fn(err => err);
 
@@ -21,16 +19,18 @@ describe('Middleware structure', () => {
   });
 });
 
-describe('Status codes', () => {
-  const handler = errorHandler(logger);
+describe('Functionality', () => {
+  describe('Status codes', () => {
+    const handler = errorHandler(logger);
 
-  test('default', () => {
-    const res = mockResponse();
+    test('default', () => {
+      const res = mockResponse();
 
-    handler(new Error('Some error'), {}, res, noop);
+      handler(new Error('Some error'), {}, res, noop);
 
-    expect(
-      res.json.calledWithMatch({ statusCode: INTERNAL_SERVER_ERROR })
-    ).toBeTruthy();
+      expect(
+        res.json.calledWithMatch({ statusCode: INTERNAL_SERVER_ERROR })
+      ).toBeTruthy();
+    });
   });
 });
